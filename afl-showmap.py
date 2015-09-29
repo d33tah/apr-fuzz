e319887c1aefa1717816dae51fc66ed937288cd9
Jacek Wielemborek <d33tah@gmail.com>
2015-09-30 01:31:31 +0200

    .

--- afl-showmap.py
+++ afl-showmap.py
@@ -3,6 +3,7 @@
 import ctypes
 import subprocess
 import atexit
+import sys
 
 IPC_PRIVATE, IPC_CREAT, IPC_EXCL, IPC_RMID = 0, 512, 1024, 0
 MAP_SIZE = 65536
@@ -17,23 +18,40 @@ def remove_shm(shm_id):
     shmctl(shm_id, IPC_RMID, 0)
 
 
-def main():
+def main(target, outfile):
+
+    sys.stderr.write("\x1b[0;36mafl-showmap \x1b[1;37m1.94b")
+    sys.stderr.write("\x1b[0m by <d33tah@gmail.com>\n")
 
     shm_id = shmget(IPC_PRIVATE, MAP_SIZE, IPC_CREAT | IPC_EXCL | 0600)
     atexit.register(remove_shm, shm_id)
 
-    print('-- Program output begins --')
-    p = subprocess.Popen('./hello', shell=True,
-                         env={'__AFL_SHM_ID': str(shm_id)})
+    target_cmd = ' '.join(target)
+    sys.stderr.write("\x1b[1;34m[*] \x1b[0mExecuting '%s'...\n" % target_cmd)
+    sys.stderr.write("\x1b[0\n")
+
+    sys.stderr.write('-- Program output begins --\n')
+    p = subprocess.Popen(target, env={'__AFL_SHM_ID': str(shm_id)})
     p.wait()
-    print('-- Program output ends --')
+    sys.stderr.write('-- Program output ends --\n')
 
     trace_bytes = shmat(shm_id, 0, 0)[0]
 
-    for i in range(MAP_SIZE):
-        if trace_bytes[i] == '\x00':
-            continue
-        print("%06u:%u" % (i, ord(trace_bytes[i])))
+    num_tuples = 0
+    with open(outfile, "w") as f:
+        for i in range(MAP_SIZE):
+            if trace_bytes[i] == '\x00':
+                continue
+            f.write("%06u:%u\n" % (i, ord(trace_bytes[i])))
+            num_tuples += 1
+
+    if num_tuples:
+        sys.stderr.write("\x1b[1;32m[+] \x1b[0mCaptured %d " % num_tuples)
+        sys.stderr.write("tuples in '%s'.\x1b[0m\n" % outfile)
+    else:
+        sys.stderr.write('\x0f\x1b)B\x1b[?25h')  # bSTOP RESET_G1 CURSOR_SHOW
+        sys.stderr.write("\n\x1b[1;31m[-] PROGRAM ABORT : \x1b")
+        sys.stderr.write("[1;37mNo instrumentation detected\x1b[1;31m\n")
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[2:], sys.argv[1])
