50cdb5c4eff61f2d7e0f15e41bfc93c34e78ab7d
Jacek Wielemborek <d33tah@gmail.com>
2015-09-30 01:07:36 +0200

    .

--- afl-showmap.py
+++ afl-showmap.py
@@ -7,15 +7,17 @@ import atexit
 IPC_PRIVATE, IPC_CREAT, IPC_EXCL, IPC_RMID = 0, 512, 1024, 0
 MAP_SIZE = 65536
 
+shmget = ctypes.cdll.LoadLibrary("libc.so.6").shmget
+shmat = ctypes.cdll.LoadLibrary("libc.so.6").shmat
+shmat.restype = ctypes.POINTER(ctypes.c_char * MAP_SIZE)
+shmctl = ctypes.cdll.LoadLibrary("libc.so.6").shmctl
+
+
 def atexit_handler(shm_id):
     shmctl(shm_id, IPC_RMID, 0)
 
-def main():
 
-    shmget = ctypes.cdll.LoadLibrary("libc.so.6").shmget
-    shmat = ctypes.cdll.LoadLibrary("libc.so.6").shmat
-    shmat.restype = ctypes.POINTER(ctypes.c_char * MAP_SIZE)
-    shmctl = ctypes.cdll.LoadLibrary("libc.so.6").shmctl
+def main():
 
     shm_id = shmget(IPC_PRIVATE, MAP_SIZE, IPC_CREAT | IPC_EXCL | 0600)
     atexit.register(atexit_handler, shm_id)
@@ -29,7 +31,8 @@ def main():
     trace_bytes = shmat(shm_id, 0, 0)[0]
 
     for i in range(MAP_SIZE):
-        if trace_bytes[i] == '\x00': continue
+        if trace_bytes[i] == '\x00':
+            continue
         print("%06u:%u" % (i, ord(trace_bytes[i])))
 
 if __name__ == '__main__':
