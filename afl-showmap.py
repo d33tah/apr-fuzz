fbff47319473a6d83f06be6431c7fb51c9b32060
Jacek Wielemborek <d33tah@gmail.com>
2015-09-30 20:33:24 +0200

    Factor out SHM instrumentation to a separate module.

--- afl-showmap.py
+++ afl-showmap.py
@@ -18,57 +18,8 @@ scripts to eliminate redundant inputs and perform other checks.
 """
 
 import argparse
-import ctypes
-import subprocess
-import atexit
 import sys
-
-IPC_PRIVATE, IPC_CREAT, IPC_EXCL, IPC_RMID = 0, 512, 1024, 0
-MAP_SIZE = 65536
-
-shmget = ctypes.cdll.LoadLibrary("libc.so.6").shmget
-shmat = ctypes.cdll.LoadLibrary("libc.so.6").shmat
-shmat.restype = ctypes.POINTER(ctypes.c_char * MAP_SIZE)
-shmctl = ctypes.cdll.LoadLibrary("libc.so.6").shmctl
-
-
-class SHMInstrumentation(object):
-
-    def __init__(self):
-        self.shm_id = None
-
-    def remove_shm(self):
-        if self.shm_id:
-            shmctl(self.shm_id, IPC_RMID, 0)
-            self.shm_id = None
-
-    def pre_proc_started(self):
-        pass
-
-    def post_proc_started(self):
-        pass
-
-    def go(self, target, outfile, infile):
-
-        shm_perms = IPC_CREAT | IPC_EXCL | 0600
-        self.shm_id = shmget(IPC_PRIVATE, MAP_SIZE, shm_perms)
-        atexit.register(self.remove_shm)
-
-        target_cmd = ' '.join(target)
-        sys.stderr.write("\x1b[1;34m[*] \x1b[0m")
-        sys.stderr.write("Executing '%s'...\n" % target_cmd)
-        sys.stderr.write("\x1b[0\n")
-
-        self.pre_proc_started()
-        p = subprocess.Popen(target, stdin=infile,
-                             env={'__AFL_SHM_ID': str(self.shm_id)})
-        p.wait()
-        self.post_proc_started()
-
-        trace_bytes = shmat(self.shm_id, 0, 0)[0]
-        shmctl(self.shm_id, IPC_RMID, 0)
-        self.shm_id = None
-        return trace_bytes
+from shm_instrumentation import SHMInstrumentation
 
 class AFLShowmap(SHMInstrumentation):
 
